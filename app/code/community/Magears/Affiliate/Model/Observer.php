fe77b8e0870c5b3e11b7ee272076550a7c3c1e83
andrey <andhy1272@gmail.com>
2017-12-06 20:44:33 -0600

    first commit

---
+++ app/code/community/Magears/Affiliate/Model/Observer.php
@@ -0,0 +1,36 @@
+<?php
+class Magears_Affiliate_Model_Observer
+{
+    const COOKIE_KEY_SOURCE = 'magears_affiliate_source';
+    
+
+    public function captureReferral(Varien_Event_Observer $observer)
+    {
+        $frontController = $observer->getEvent()->getFront();
+
+        $utmSource = $frontController->getRequest()
+            ->getParam('utm_source', false);
+
+        if ($utmSource) {
+            Mage::getModel('core/cookie')->set(
+                self::COOKIE_KEY_SOURCE, 
+                $utmSource, 
+                $this->_getCookieLifetime()
+            );
+        }
+    }
+    
+
+    protected function _getCookieLifetime()
+    {
+        $days = Mage::getStoreConfig(
+            'magears_affiliate/cookie/timeout'
+        );
+
+        // convert to seconds
+        return (int)86400 * $days;
+    }
+    
+    
+    
+}
---
+++ app/code/local/Magentojack/Custom/sql/custom_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,32 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->run("
+
+CREATE TABLE IF NOT EXISTS {$this->getTable('sales_quote_custom')} (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `quote_id` int(11) unsigned NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS {$this->getTable('sales_order_custom')} (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `order_id` int(11) unsigned NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `sales_flat_quote` ADD `refferal_id` VARCHAR(50);
+
+ALTER TABLE `sales_flat_order` ADD `refferal_id` VARCHAR(50);
+
+");
+
+$installer->endSetup(); 
